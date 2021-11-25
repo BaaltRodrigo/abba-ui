41f9611508bbf4230c9a63ac87f9864f32bd9b2d
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2021-11-25 16:03:33 -0300

    refactor: :truck: Rename Home to Root

---
+++ src/views/Root.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <app-header />
+    <app-sidebar />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+import AppHeader from '../components/AppHeader.vue'
+import AppSidebar from '../components/AppSidebar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    AppHeader,
+    AppSidebar
+  }
+}
+</script>
