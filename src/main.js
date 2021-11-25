2b3191b40047f951d425dd3056ac00faf7493694
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2021-11-25 01:02:24 -0300

    Start using VueCookies

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import store from './store'
 import router from './router'
+import VueCookies from 'vue-cookies';
 
 Vue.config.productionTip = false
+Vue.use(VueCookies);
 
 new Vue({
   vuetify,
